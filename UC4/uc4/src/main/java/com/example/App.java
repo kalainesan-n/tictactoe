d9e83fe6fb6f22b5494a498d7d5effc137305599
kalainesan-n <kalainesannatarajan@gmail.com>
2026-04-21 11:10:04 +0530

    UC5: move validation

--- UC4/uc4/src/main/java/com/example/App.java
+++ UC4/uc4/src/main/java/com/example/App.java
@@ -3,7 +3,14 @@ package com.example;
 import java.util.Scanner;
 
 public class App {
+
     public static void main(String[] args) {
+
+        char[][] board = {
+                {'-', '-', '-'},
+                {'-', '-', '-'},
+                {'-', '-', '-'}
+        };
 
         Scanner sc = new Scanner(System.in);
 
@@ -13,7 +20,38 @@ public class App {
         int row = (slot - 1) / 3;
         int col = (slot - 1) % 3;
 
-        System.out.println("Row: " + row);
-        System.out.println("Col: " + col);
+        if (isValidMove(board, row, col)) {
+            board[row][col] = 'X';
+            System.out.println("Move accepted!");
+        } else {
+            System.out.println("Invalid move!");
+        }
+
+        printBoard(board);
+    }
+
+    // UC5 validation
+    public static boolean isValidMove(char[][] board, int row, int col) {
+
+        // check bounds
+        if (row < 0 || row > 2 || col < 0 || col > 2) {
+            return false;
+        }
+
+        // check empty
+        if (board[row][col] != '-') {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void printBoard(char[][] board) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }

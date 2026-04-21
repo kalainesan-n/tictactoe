92dc833bf9bde5ee35342e2e0a0522724e7683eb
kalainesan-n <kalainesannatarajan@gmail.com>
2026-04-21 10:43:03 +0530

    UC3: user input for slot

--- UC2/uc2/src/main/java/com/example/App.java
+++ UC2/uc2/src/main/java/com/example/App.java
@@ -1,43 +1,22 @@
 package com.example;
 
-import java.util.Random;
+import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
 
-        // Players
-        String player1 = "Player 1";
-        String player2 = "Player 2";
+        int slot = getUserInput();
 
-        // Symbols
-        char player1Symbol;
-        char player2Symbol;
+        System.out.println("You selected slot: " + slot);
+    }
 
-        // Random toss
-        Random random = new Random();
-        int toss = random.nextInt(2); // 0 or 1
+    public static int getUserInput() {
+        Scanner scanner = new Scanner(System.in);
 
-        String currentPlayer;
-        char currentSymbol;
+        System.out.print("Enter a slot (1-9): ");
+        int slot = scanner.nextInt();
 
-        if (toss == 0) {
-            currentPlayer = player1;
-            player1Symbol = 'X';
-            player2Symbol = 'O';
-            currentSymbol = 'X';
-        } else {
-            currentPlayer = player2;
-            player2Symbol = 'X';
-            player1Symbol = 'O';
-            currentSymbol = 'X';
-        }
-
-        // Output
-        System.out.println("Toss Result:");
-        System.out.println(currentPlayer + " starts first!");
-        System.out.println(player1 + " symbol: " + player1Symbol);
-        System.out.println(player2 + " symbol: " + player2Symbol);
-        System.out.println("Current Player: " + currentPlayer + " (" + currentSymbol + ")");
+        return slot;
     }
 }
